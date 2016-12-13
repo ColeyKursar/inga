44d0cdae5de35ca9663b4f970215ca17d92f0e74
Zach Zundel <16zzundel5@gmail.com>
2016-12-13 09:29:43 -0700

    Added stderr pipelining

--- kursarcoley/update/views.py
+++ kursarcoley/update/views.py
@@ -7,4 +7,4 @@ def UpdateView(request):
     git_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
     git_pull = ['git', 'pull']
 
-    subprocess.check_output(git_pull, cwd=git_dir, universal_newlines=True)
+    subprocess.check_output(git_pull, cwd=git_dir, stderr=subprocess.STDOUT, universal_newlines=True)
