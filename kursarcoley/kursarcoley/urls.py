faab88c406bc52f784ba39dfb8bb1ea04f4956e1
Zach Zundel <16zzundel5@gmail.com>
2016-12-13 09:20:30 -0700

    Fixed URLS

--- kursarcoley/kursarcoley/urls.py
+++ kursarcoley/kursarcoley/urls.py
@@ -19,5 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^web/', include("ingaweb.urls")),
-    url(r'^csv/', include("csvimport.urls"))
+    url(r'^update/', include("update.urls")),
 ]
--- kursarcoley/update/urls.py
+++ kursarcoley/update/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from updater.views import UpdateView
 
 urlpatterns = [
-    url(r'^update/', UpdateView),
+    url(r'^/', UpdateView),
 ]
