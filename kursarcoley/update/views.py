2f887849eecd6d13c76b3ac14da7499c041ab75a
Zach Zundel <16zzundel5@gmail.com>
2016-12-13 09:51:14 -0700

    Whoami test

--- kursarcoley/update/views.py
+++ kursarcoley/update/views.py
@@ -7,4 +7,6 @@ def UpdateView(request):
     git_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
     git_pull = "git pull"
 
+    subprocess.check_output("whoami", cwd=git_dir, stderr=subprocess.STDOUT, universal_newlines=True, shell=True)
+
     subprocess.check_output(git_pull, cwd=git_dir, stderr=subprocess.STDOUT, universal_newlines=True, shell=True)
