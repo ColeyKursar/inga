3872cccec56f402d259036ad2b311eacf02bf7eb
Zach Zundel <16zzundel5@gmail.com>
2016-12-13 09:23:42 -0700

    View fixes

--- kursarcoley/update/views.py
+++ kursarcoley/update/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
-from os import path
+import os
 import subprocess
 
 
-class UpdateView(request):
-    git_dir = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..'))
+class UpdateViews:
+    git_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
     git_pull = ['git', 'pull']
 
     subprocess.run(git_pull, cwd=git_dir, universal_newlines=True)
