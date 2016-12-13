b7e1b07b5fba70a13d4b4d8583b993d3a53c0a75
Zach Zundel <16zzundel5@gmail.com>
2016-12-13 10:00:39 -0700

    Removed dict

--- kursarcoley/update/views.py
+++ kursarcoley/update/views.py
@@ -7,6 +7,6 @@ def UpdateView(request):
     git_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
     git_pull = "git pull"
 
-    identity = subprocess.check_output("whoami", cwd=git_dir, stderr=subprocess.STDOUT, universal_newlines=True, shell=True).__dict__
+    identity = subprocess.check_output("whoami", cwd=git_dir, stderr=subprocess.STDOUT, universal_newlines=True, shell=True)
 
     subprocess.check_output(git_pull, cwd=git_dir, stderr=subprocess.STDOUT, universal_newlines=True, shell=True)
