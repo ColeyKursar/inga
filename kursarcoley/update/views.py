dd8f893d1c8b37e24d929d2972b804a5ea9a3e44
Zach Zundel <16zzundel5@gmail.com>
2016-12-13 09:59:35 -0700

    Added identity

--- kursarcoley/update/views.py
+++ kursarcoley/update/views.py
@@ -7,6 +7,6 @@ def UpdateView(request):
     git_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
     git_pull = "git pull"
 
-    subprocess.check_output("whoami", cwd=git_dir, stderr=subprocess.STDOUT, universal_newlines=True, shell=True)
+    identity = subprocess.check_output("whoami", cwd=git_dir, stderr=subprocess.STDOUT, universal_newlines=True, shell=True).__dict__
 
     subprocess.check_output(git_pull, cwd=git_dir, stderr=subprocess.STDOUT, universal_newlines=True, shell=True)
